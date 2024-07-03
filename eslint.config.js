3e0c5c2bc13e659f745d84ab1deeed92ee737197
Mark Lopez <m@silvenga.com>
2024-07-03 11:15:32 -0500

    Migrated away from deprecated eslint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -2,37 +2,48 @@
 
 import "eslint-plugin-only-warn";
 
-import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js"
-import reactRecommended from "eslint-plugin-react/configs/recommended.js"
-import typeScriptEslint from "typescript-eslint";
 import eslint from "@eslint/js";
+import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import stylistic from "@stylistic/eslint-plugin";
+import typeScriptEslint, { } from "typescript-eslint";
 
 export default typeScriptEslint.config(
-  eslint.configs.recommended,
-  ...typeScriptEslint.configs.recommendedTypeChecked,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    ...reactRecommended,
-    ...reactJsxRuntime, // Ignore react-in-jsx-scope warnings.
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    eslint.configs.recommended,
+    stylistic.configs["disable-legacy"], // Disable legacy eslint-proper rules.
+    ...typeScriptEslint.configs.recommendedTypeChecked,
+    stylistic.configs["recommended-flat"],
+
+    // @ts-expect-error Bad typings from stylistic.
+    stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        indent: 4
+    }),
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    {
+        files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+        ...reactRecommended,
+        ...reactJsxRuntime, // Ignore react-in-jsx-scope warnings.
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
+    {
+        files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "default-case": "warn",
+            "require-await": "warn",
+        }
     },
-    rules: {
-      "quotes": "warn",
-      "default-case": "warn",
-      "require-await": "warn",
-    }
-  },
 );
