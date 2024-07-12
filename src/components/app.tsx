ac870ecd9a60f14e46d0250681dc6dd01fc8ec3e
Mark Lopez <m@silvenga.com>
2024-07-11 21:03:51 -0500

    Completed sidebar.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,10 @@
 import clsx from "clsx";
 import { DateTime } from "luxon";
 import { useMemo, useState } from "react";
+import { IoCodeDownloadOutline } from "react-icons/io5";
+import { TbJson } from "react-icons/tb";
+import { TbAppWindow } from "react-icons/tb";
+import { getResume } from "../data/resume";
 import { PdfViewer } from "./pdf-viewer";
 import { ResumeDocument } from "./resume/document";
 import { ResumeContextProvider } from "./resume/use-resume";
@@ -34,24 +38,42 @@ export function App() {
 
 function SideMenu({ pdfBlobUrl, loadDurationMs }: { pdfBlobUrl?: string; loadDurationMs: number }) {
     const year = useMemo(() => DateTime.now().year, []);
+    const jsonUrl = useMemo(() => {
+        const resume = getResume();
+        const json = JSON.stringify(resume, undefined, 2);
+        return URL.createObjectURL(new Blob([json], {
+            type: "application/json"
+        }));
+    }, []);
     const disabled = !pdfBlobUrl;
     return (
-        <aside className="md:ms-9 mb-4 md:mb-0 w-[100%] md:w-[300px] self-start flex flex-col select-none" role="banner">
+        <aside className="md:ms-9 mb-4 md:mb-0 w-[100%] md:w-max self-start flex flex-col select-none text-center" role="banner">
             <a
-                className={clsx("mb-3 p-3 rounded bg-white border drop-shadow-sm transition-all hover:bg-gray-100 active:bg-gray-200", disabled && "opacity-50 pointer-events-none cursor-default")}
+                className={clsx("flex items-center mb-3 py-3 px-4 rounded bg-white border drop-shadow-sm transition-all hover:bg-gray-100 active:bg-gray-200", disabled && "opacity-50 pointer-events-none cursor-default")}
                 role="button"
                 href={pdfBlobUrl}
                 target="_blank">
-                Open in System PDF Reader
+                <TbAppWindow style={{ width: 24, height: 24 }} className="me-3" />Open in System PDF Reader
             </a>
-            <a
-                className={clsx("mb-3 p-3 rounded bg-white border drop-shadow-sm transition-all hover:bg-gray-100 active:bg-gray-200", disabled && "opacity-50 pointer-events-none cursor-default")}
-                role="button"
-                href={pdfBlobUrl}
-                download={`Resume Mark Lopez ${year}.pdf`}
-                type="application/pdf">
-                Save PDF
-            </a>
+            <div className="flex">
+                <a
+                    className={clsx("flex items-center grow mb-3 py-3 px-4 rounded bg-white border drop-shadow-sm transition-all hover:bg-gray-100 active:bg-gray-200", disabled && "opacity-50 pointer-events-none cursor-default")}
+                    role="button"
+                    href={pdfBlobUrl}
+                    download={`Mark Lopez ${year}.pdf`}
+                    type="application/pdf">
+                    <IoCodeDownloadOutline style={{ width: 24, height: 24 }} className="me-auto" /> Save PDF
+                </a>
+                <a
+                    className={clsx("ms-3 mb-3 py-3 px-4 rounded bg-white border drop-shadow-sm transition-all hover:bg-gray-100 active:bg-gray-200", disabled && "opacity-50 pointer-events-none cursor-default")}
+                    role="button"
+                    href={jsonUrl}
+                    aria-label="Download Json"
+                    download={`mark-lopez-${year}.json`}
+                    type="application/pdf">
+                    <TbJson style={{ width: 24, height: 24 }} />
+                </a>
+            </div>
             <div className="text-center">
                 <a href="https://silvenga.com" className="text-center hover:underline font-medium">
                     Back to silvenga.com
