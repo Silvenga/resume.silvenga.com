0f911d68deb2783fda4f91a3a8d6eca42bc07cac
Mark Lopez <m@silvenga.com>
2026-08-18 11:05:32 -0500

    chore: updated handling of mobile viewports

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -14,18 +14,25 @@ export function SideMenu({ pdfBlobUrl, renderTimeMs }: SideMenuProps) {
   const year = useYear();
   return (
     <header className="mb-4 h-full w-full max-w-full text-center whitespace-nowrap select-none md:ms-9 md:mb-0 md:w-60">
-      <div className="flex flex-col gap-3 md:fixed md:h-[calc(100vh-8rem)] md:w-60">
+      <div className="flex flex-row gap-3 md:fixed md:h-[calc(100vh-8rem)] md:w-60 md:flex-col">
         <LinkButton
           variant="primary"
           href={pdfBlobUrl}
           download={`Mark Lopez ${year}.pdf`}
           type="application/pdf"
           rel="nofollow"
+          className="w-full"
         >
           <IoCodeDownloadOutline className="h-6 w-6" />
           Download PDF
         </LinkButton>
-        <LinkButton variant="ghost" target="_blank" href={SourceCode} aria-label="Source Code">
+        <LinkButton
+          variant="ghost"
+          target="_blank"
+          href={SourceCode}
+          aria-label="Source Code"
+          className="w-full"
+        >
           <FaGithub className="h-6 w-6" />
           Source Code
         </LinkButton>
