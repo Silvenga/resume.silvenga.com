ee6c9bde54ca1071cc1418ba1fc4b7a34aaffc92
Mark Lopez <m@silvenga.com>
2026-08-18 11:02:27 -0500

    chore: updated handling of dark mode

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -2,8 +2,8 @@ import type { AnchorHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 const variants = {
-  primary: twMerge("bg-gray-900 hover:bg-gray-600 text-white"),
-  ghost: twMerge("bg-white hover:bg-gray-100 border-gray-900 border"),
+  primary: twMerge("bg-gray-900 dark:bg-gray-600 hover:bg-gray-600 dark:hover:bg-gray-500 text-white"),
+  ghost: twMerge("hover:bg-gray-100 dark:hover:bg-gray-800 border-gray-900 dark:border-gray-400 border"),
 };
 
 type LinkButtonProps = {
